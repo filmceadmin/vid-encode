2cfbeaf86b42a91bc0bc538662809267766bd4c9
abdulkader07890 <abdulkader07890@gmail.com>
2021-06-13 22:26:37 +0600

    all files

---
+++ README.md
@@ -0,0 +1,33 @@
+# Video Encoder Bot
+A Telegram bot to convert videos into x265/x264 format via ffmpeg.
+
+<p><a href="https://heroku.com/deploy"> <img src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku" width="200""/></a></p>
+
+
+
+
+### Configuration
+Add values in environment variables or add them in [config.env.template](./config.env.template) and rename file to `config.env`.
+- `API_ID` - Get it by creating an app on [https://my.telegram.org](https://my.telegram.org)
+- `API_HASH` - Get it by creating an app on [https://my.telegram.org](https://my.telegram.org)
+- `BOT_TOKEN` - Get it by creating a bot on [https://t.me/BotFather](https://t.me/BotFather)
+- `SUDO_USERS` - Chat identifier of the sudo user. For multiple users use space as seperator.
+- `DOWNLOAD_DIR` - (Optional) Temporary download directory to keep downloaded files.
+
+### Configuring Encoding Format
+To change the ffmpeg profile edit them in [ffmpeg_utils.py](/bot/helper/ffmpeg_utils.py)
+
+### Installing Requirements
+Install the required Python Modules in your machine.
+```sh
+apt-get -qq install ffmpeg
+pip3 install -r requirements.txt
+```
+### Deployment
+With python3.7 or later.
+```sh
+python3 main.py
+```
+
+### Copyright & License
+- Licensed under the terms of the [GNU General Public License Version 3 &dash; 29 June 2007](./LICENSE)
---
+++ bot/helper/ffmpeg_utils.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import json
+import time
+import ffmpeg
+from subprocess import call, check_output
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+
+def get_codec(filepath, channel='v:0'):
+    output = check_output(['ffprobe', '-v', 'error', '-select_streams', channel,
+                            '-show_entries', 'stream=codec_name,codec_tag_string', '-of', 
+                            'default=nokey=1:noprint_wrappers=1', filepath])
+    return output.decode('utf-8').split()
+
+def encode(filepath):
+    basefilepath, extension = os.path.splitext(filepath)
+    output_filepath = basefilepath + ' 10bit x265' + '.mkv'
+    assert(output_filepath != filepath)
+    if os.path.isfile(output_filepath):
+        print('Skipping "{}": file already exists'.format(output_filepath))
+        return None
+    print(filepath)
+    # Get the video channel codec
+    video_codec = get_codec(filepath, channel='v:0')
+    if video_codec == []:
+        print('Skipping: no video codec reported')
+        return None
+    # Video transcode options
+    video_opts = '-c:v libx265 -preset ultrafast -pix_fmt yuv420p10le -threads 0'
+    
+    # Get the audio channel codec
+    audio_opts = '-c:a aac -b:a 128k'
+    call(['ffmpeg', '-i', filepath] + video_opts.split() + audio_opts.split() + [output_filepath])
+    os.remove(filepath)
+    return output_filepath
+
+def get_thumbnail(in_filename, path, ttl):
+    out_filename = os.path.join(path, str(time.time()) + ".jpg")
+    open(out_filename, 'a').close()
+    try:
+        (
+            ffmpeg
+            .input(in_filename, ss=ttl)
+            .output(out_filename, vframes=1)
+            .overwrite_output()
+            .run(capture_stdout=True, capture_stderr=True)
+        )
+        return out_filename
+    except ffmpeg.Error as e:
+      return None
+
+def get_duration(filepath):
+    metadata = extractMetadata(createParser(filepath))
+    if metadata.has("duration"):
+      return metadata.get('duration').seconds
+    else:
+      return 0
+
+def get_width_height(filepath):
+    metadata = extractMetadata(createParser(filepath))
+    if metadata.has("width") and metadata.has("height"):
+      return metadata.get("width"), metadata.get("height")
+    else:
+      return 1280, 720
